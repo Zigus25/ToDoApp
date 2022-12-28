4bf7204c79c32e5b4eff5a535876dc97a0ec6187
MaZy <maciej.zet004@gmail.com>
2022-12-28 19:38:53 +0100

    Schedule update

--- app/src/main/java/pl/mazy/todoapp/ui/components/events/SingleEvent.kt
+++ app/src/main/java/pl/mazy/todoapp/ui/components/events/SingleEvent.kt
@@ -1,2 +1,42 @@
 package pl.mazy.todoapp.ui.components.events
 
+import android.graphics.Color.parseColor
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import pl.mazy.todoapp.Schedule
+import pl.mazy.todoapp.ui.theme.ToDoAPpTheme
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun SingleEvent(schedule:Schedule){
+    ToDoAPpTheme {
+        val formatter = DateTimeFormatter.ofPattern("HH:mm")
+        val time1 = LocalTime.parse(schedule.TimeStart, formatter)
+        val time2 = LocalTime.parse(schedule.TimeEnd, formatter)
+        val duration = (time1.until(time2, ChronoUnit.MINUTES)).toInt()
+        Column(modifier = Modifier.height((duration*2).dp)) {
+            Row(modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 15.dp, end = 15.dp)
+                .background(
+                    Color(parseColor(schedule.Color))
+                )) {
+                Text(text = schedule.Name)
+            }
+        }
+    }
+}
--- app/src/main/java/pl/mazy/todoapp/ui/views/Schedule.kt
+++ app/src/main/java/pl/mazy/todoapp/ui/views/Schedule.kt
@@ -1,5 +1,8 @@
 package pl.mazy.todoapp.ui.views
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -7,51 +10,59 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import pl.mazy.todoapp.logic.navigation.Destinations
 import pl.mazy.todoapp.logic.navigation.NavController
+import pl.mazy.todoapp.ui.components.events.SingleEvent
+import pl.mazy.todoapp.ui.theme.ToDoAPpTheme
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Schedule(
     navController: NavController<Destinations>,
 ){
-    Column(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.weight(1f)){
-
-        }
-        BottomAppBar {
-            IconButton(onClick = { /* doSomething() */ }) {
-                Icon(Icons.Filled.Menu, contentDescription = "Menu Icon")
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            Box(modifier = Modifier.weight(1f)) {
-                SmallFloatingActionButton(
-                    onClick = { navController.navigate(Destinations.CreateNote) },
-                    modifier = Modifier
-                        .height(50.dp)
-                        .width(50.dp)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Add,
-                        contentDescription = null,
-                    )
+    ToDoAPpTheme {
+        Column(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+            LazyColumn(modifier = Modifier.weight(1f)){
+                item {
+                    SingleEvent(pl.mazy.todoapp.Schedule("Sample Event","Lorem Ipsum this is my test description","09:00","10:00","12","0","#35fa12"))
                 }
             }
+            BottomAppBar {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Filled.Menu, contentDescription = "Menu Icon")
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Box(modifier = Modifier.weight(1f)) {
+                    SmallFloatingActionButton(
+                        onClick = { navController.navigate(Destinations.CreateNote) },
+                        modifier = Modifier
+                            .height(50.dp)
+                            .width(50.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = null,
+                        )
+                    }
+                }
 
-            IconButton(onClick = { navController.navigate(Destinations.TaskList) }) {
-                Icon(
-                    Icons.Filled.Checklist,
-                    contentDescription = "Calendar Icon",
-                )
-            }
-            IconButton(onClick = { navController.navigate(Destinations.Notes) }) {
-                Icon(
-                    Icons.Filled.Edit,
-                    contentDescription = "Calendar Icon",
-                )
+                IconButton(onClick = { navController.navigate(Destinations.TaskList) }) {
+                    Icon(
+                        Icons.Filled.Checklist,
+                        contentDescription = "Calendar Icon",
+                    )
+                }
+                IconButton(onClick = { navController.navigate(Destinations.Notes) }) {
+                    Icon(
+                        Icons.Filled.Edit,
+                        contentDescription = "Calendar Icon",
+                    )
+                }
             }
         }
     }
